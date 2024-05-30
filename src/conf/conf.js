6acd2b611aece1ac2f91b5ecd2cd63cea10582bf
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-05-31 00:22:56 +0530

    RTE API added

--- src/conf/conf.js
+++ src/conf/conf.js
@@ -4,7 +4,7 @@ const conf = {
     appwriteDatabaseId: String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     appwriteCollectionId: String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     appwriteBucketId: String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
-    tinyMCEAPIKey: String(import.meta.env.VITE_TINYMCE_API_KEY),
+    tinyMCEAPIKey: String(import.meta.env.VITE_TINYMCE),
 }
 
 export default conf
