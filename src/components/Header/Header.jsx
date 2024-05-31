858688e5d50b97be9f4769db4e4c4f5589b51a8a
Aditya Kumar Kushwaha <akkpk933@gmail.com>
2024-05-31 12:19:14 +0530

    Header Updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Logo, LogoutBtn } from "../index";
 import { Link } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -7,6 +7,11 @@ import { useNavigate } from "react-router-dom";
 function Header() {
   const authStatus = useSelector((state) => state.auth.status);
   const navigate = useNavigate();
+  const [isHide, setIsHide] = useState(true);
+
+  const handleClick = () => {
+    setIsHide(!isHide);
+  }
 
   const navItems = [
     {
@@ -39,13 +44,13 @@ function Header() {
   return (
     <header className="py-3 bg-gray-900 shadow">
       <Container>
-        <nav className="flex">
+        <nav className="flex relative justify-between items-center">
           <div className="mr-4">
             <Link to="/">
               <Logo width="50px" />
             </Link>
           </div>
-          <ul className="flex ml-auto gap-1">
+          <ul className="md:flex ml-auto gap-1 hidden">
             {navItems.map( (item) => 
               item.active ? (
                 <li key={item.name}>
@@ -62,6 +67,29 @@ function Header() {
               </li>
             )}
           </ul>
+
+          <button onClick={handleClick} className="md:hidden">
+            {isHide ? <img src="menu.svg" alt="menu-icon" /> : <img src="x.svg" alt="x-icon" />}
+          </button>
+          {isHide ? "" : 
+            <ul className="absolute flex flex-col w-40 gap-1 md:hidden top-16 right-2 border rounded-xl duration-200 p-1">
+              {navItems.map( (item) => 
+                item.active ? (
+                  <li key={item.name}>
+                    <button
+                      onClick={() => {navigate(item.slug)}}
+                      className="inline-bock px-6 py-2 duration-200 hover:text-blue-400 rounded-xl hover:bg-gray-600 w-full"
+                    >{item.name}</button>
+                  </li>
+                ) : null
+              )}
+              { authStatus && (
+                <li className="hover:bg-gray-600 rounded-xl flex justify-center w-full">
+                  <LogoutBtn />
+                </li>
+              )}
+            </ul>
+          }
         </nav>
       </Container>
     </header>
